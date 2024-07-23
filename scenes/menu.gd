c07dec03bf7f5366d4aa8ebc8f5f365af84a019e
Noah May <noahmouse2011@gmail.com>
2024-07-23 05:42:43 -0500

    I think I know how to handle levels

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -1,16 +1,12 @@
 extends Node
 
+@export var levels: Array[PackedScene]
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	RenderingServer.set_default_clear_color(Color.BLACK)
-	$StartButton.grab_focus()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+	$VBoxContainer/StartButton.grab_focus()
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://level.tscn")
+	get_tree().change_scene_to_packed(levels[0])
+
+func _on_quit_button_pressed():
+	get_tree().quit(0)
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,43 +1,6 @@
 extends CharacterBody2D
 
 @onready var ray_cast_2d = $RayCast2D
-@onready var timer = $Timer
-
-var width = 9
-
-func _ready():
-	var viewport
-	viewport = get_viewport()
-	print(viewport.content_scale_size)
-	viewport.content_scale_factor = 960.0 / 144.0
-	#viewport.content_scale_size = Vector2i(144, 144)
-	# viewport.size
-	#print(viewport == get_window().get_viewport())
-	#print(get_viewport().size_2d_override)
-	# get_viewport().size = Vector2i(4, 4) * width * Globals.grid_scale
-	#print(viewport.content_scale_mode, ContentScale)
-
-	print("test")
-	
-	#viewport.set_global_canvas_transform(viewport.global_canvas_transform.scaled(Vector2(.9, .9)))
-	#viewport.get_window().wid
-	#viewport.size = Vector2(36, 36) * Globals.grid_scale
-	#get_window().size = Vector2i(4, 4) * width * Globals.grid_scale
-	#viewport.set_global_canvas_transform(viewport.global_canvas_transform.scaled(Vector2(.9, .9)))
-
-func _on_timer_timeout():
-	#get_window().size = Vector2i(4, 4) * width * Globals.grid_scale
-	#viewport.size
-	width += 1
-
-#func _ready():
-	#var tilemap: TileMap = get_parent().get_node("TileMap")
-	#var rect = tilemap.get_used_rect()
-	#var tile_size = tilemap.tile_set.tile_size
-	#$Camera2D.limit_top = rect.position.y * tile_size.y
-	#$Camera2D.limit_right = rect.end.x * tile_size.x
-	#$Camera2D.limit_bottom = rect.end.y * tile_size.y
-	#$Camera2D.limit_left = rect.position.x * tile_size.x
 
 func _unhandled_key_input(event):
 	for dir in Globals.direction_map.keys():
